7a8fd3937220871ba2d295ab7d8046cdb7c6b55d
n0skll3r@gmail.com <n0skll3r@gmail.com>
2018-03-05 22:11:47 +0100

    # added forgotten file

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+//set for Heroku deployment
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(process.env.PORT || 4200, () => console.log(`App listening on port ${process.env.PORT} and 4200!`));
