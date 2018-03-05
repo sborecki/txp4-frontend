adeff9814dbed9c99647ce85f389c408d9dfbe1d
n0skll3r@gmail.com <n0skll3r@gmail.com>
2018-03-05 22:06:26 +0100

    # FIxes for heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { MainComponent } from './main/main.component';
 const routes: Routes = [
 
   { path: '', component: MainComponent },
-  { path: ':playerLogin', component: MainComponent }
+  { path: 'user/:playerLogin', component: MainComponent }
 ];
 
 @NgModule({
@@ -13,8 +13,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule {
-
-
-
 
 }
